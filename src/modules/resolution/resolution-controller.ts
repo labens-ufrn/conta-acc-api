acf8fc1ed60482a151866b7796dae6364689bbd5
Everton Fernandes <evertonfrnds@gmail.com>
2023-11-29 23:23:07 -0300

    Update resolution routes and controller

--- src/modules/resolution/resolution-controller.ts
+++ src/modules/resolution/resolution-controller.ts
@@ -1,9 +1,10 @@
 import p from "pomme-ts";
 import { v1ListResolution } from "./v1-list-resolution";
 import { v1CreateResolution } from "./v1-create-resolution";
+import { v1UpdateResolution } from "./v1-update-resolution";
 
 export const resolutionController = p
   .controller()
   .withPath("/resolution")
-  .withRoutes([v1ListResolution, v1CreateResolution])
+  .withRoutes([v1ListResolution, v1CreateResolution, v1UpdateResolution])
   .build();
---
+++ src/modules/resolution/v1-update-resolution.ts
@@ -0,0 +1,45 @@
+import p from "pomme-ts";
+import { z } from "zod";
+import { Prisma } from "@prisma/client";
+import { resolutionModel } from "./model-resolution";
+
+const bodySchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  link: z.string().optional(),
+});
+
+export const v1UpdateResolution = p.route.put({
+  key: "updateResolution",
+  path: "/:id",
+  bodySchema,
+  async resolver({ body, params }, ctx) {
+    const { name, description, link } = body;
+    const { id } = params;
+
+    const resolutionFound = await resolutionModel.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!resolutionFound) {
+      p.error.badRequest("Resolution not found");
+    }
+
+    const resolution = await resolutionModel.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        description,
+        link,
+      },
+    });
+
+    return {
+      resolution,
+    };
+  },
+});
